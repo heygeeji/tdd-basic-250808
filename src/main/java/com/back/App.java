f4513ad09a614379f0d3e84cd4d1806e1008130d
heygeeji <heygeeji@gmail.com>
2025-08-08 14:28:39 +0900

    step green

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class App {
+package com.back;
+
+public class App {
+    public int plus(int num1, int num2) {//2~3번 하면 구조가 보임
+        if (num1 == 1 && num2 == 2) {
+            return 3;
+        }
+        if (num1 == 0 && num2 == 5) {
+            return 5;
+        }
+
+        return 30;
+    }
 }
