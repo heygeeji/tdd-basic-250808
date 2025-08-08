0fb1f1edbc6f37417c05d2207d8d3a4ad3387131
heygeeji <heygeeji@gmail.com>
2025-08-08 14:29:34 +0900

    step blue

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -2,13 +2,6 @@ package com.back;
 
 public class App {
     public int plus(int num1, int num2) {//2~3번 하면 구조가 보임
-        if (num1 == 1 && num2 == 2) {
-            return 3;
-        }
-        if (num1 == 0 && num2 == 5) {
-            return 5;
-        }
-
-        return 30;
+        return num1 + num2;
     }
 }
